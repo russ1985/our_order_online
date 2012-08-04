f6f724a736498c95ff5f92095bda01825c1252c0
unknown <russellfholmes@gmail.com>
2012-08-04 10:38:25 -0400

    removed all but engine

--- config/initializers/mail_setup.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'russedge.com',
-  :user_name            => 'russedgesite@gmail.com',
-  :password             => '3odzilla',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true 
-}
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Russedge::Application.config.secret_token = '50af6078493df62cd62ed823cea42ddb2559c3d153e38c70bd5881409a770e657df4bcbef005faad52b87b63eb6515e0b4511e0aead641cb6c96f3b5d68ae838'
